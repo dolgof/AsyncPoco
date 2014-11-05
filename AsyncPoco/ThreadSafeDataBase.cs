7932f45e50b5e6ea15442b75d30fb9c836c81e6a
Александр Долгов <a.dolgov@live.ru>
2014-11-05 15:20:30 +0300

    Threadsafe wrapper for DataBase class added

---
+++ AsyncPoco/ThreadSafeDataBase.cs
@@ -0,0 +1,47 @@
+﻿/* AsyncPoco is a fork of PetaPoco and is bound by the same licensing terms.
+ *
+ * PetaPoco - A Tiny ORMish thing for your POCO's.
+ * Copyright © 2011-2012 Topten Software.  All Rights Reserved.
+ * 
+ * Apache License 2.0 - http://www.toptensoftware.com/petapoco/license
+ * 
+ * Special thanks to Rob Conery (@robconery) for original inspiration (ie:Massive) and for 
+ * use of Subsonic's T4 templates, Rob Sullivan (@DataChomp) for hard core DBA advice 
+ * and Adam Schroder (@schotime) for lots of suggestions, improvements and Oracle support
+ */
+
+// Define PETAPOCO_NO_DYNAMIC in your project settings on .NET 3.5
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncPoco
+{
+	/// <summary>
+	/// Threadsafe wrapper for AsyncPoco.Database
+	/// </summary>
+	public class ThreadSafeDataBase : Database
+	{
+		readonly SemaphoreSlim _connectionSyncLock = new SemaphoreSlim(1);
+
+		public ThreadSafeDataBase(string connectionStringName)
+			: base(connectionStringName)
+		{
+		}
+
+		/// <summary>
+		/// Threadsafe open shared connection
+		/// </summary>
+		/// <remarks>
+		/// base.OpenSharedConnectionAsync() must be vitrtual.
+		/// </remarks>
+		public override async Task OpenSharedConnectionAsync()
+		{
+			await _connectionSyncLock.WaitAsync();
+ 
+			await base.OpenSharedConnectionAsync();
+
+			_connectionSyncLock.Release();
+		}
+	}
+}
